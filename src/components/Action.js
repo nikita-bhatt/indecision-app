5a5eb40632b5de2f70bebe8289b3a3dc01a65dc9
Nikita Bhatt <nikitabhattpth@gmail.com>
2022-03-09 17:57:23 +0800

    Refactoring all stateless components

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,15 +1,13 @@
 import React from 'react'
 
 const Action = (props) =>
-{
-    return (
-        <div>
-            <button 
-            onClick={props.handlePick}
-            disabled={!props.hasOption}> 
-            What should I Do?
-            </button>
-        </div>
-    );
-}
+(
+    <div>
+        <button 
+        onClick={props.handlePick}
+        disabled={!props.hasOption}> 
+        What should I Do?
+        </button>
+    </div>
+);
 export default Action;
