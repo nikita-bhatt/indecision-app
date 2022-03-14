fd55d55f559daa921ca31f6d5e60224d48086eb5
Nikita Bhatt <nikitabhattpth@gmail.com>
2022-03-14 15:10:59 +0800

    Added button styles

--- src/components/Action.js
+++ src/components/Action.js
@@ -4,6 +4,7 @@ const Action = (props) =>
 (
     <div>
         <button 
+        className= "big-button"
         onClick={props.handlePick}
         disabled={!props.hasOption}> 
         What should I Do?
